a684e9fc37ebf90ea0327ecfc0958cc25b2c2a26
gonzalodiazcanavate <gonzalodiazca@gmail.com>
2026-06-13 01:36:25 +0200

    refactor: Nombre del backend modificado

--- backend/src/test/java/com/webllope/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/webllope/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.webllope.chollometro;
+package com.webllope.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
